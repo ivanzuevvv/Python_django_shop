a0d08c4c36be3c4d07cb9755f82ea12e7c362baf
Андрей Зайцев <mr_zaycev@mail.ru>
2023-01-10 13:21:46 +0000

    Replace urls.py

--- project_aggregator/project_aggregator/urls.py
+++ project_aggregator/project_aggregator/urls.py
@@ -21,5 +21,5 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/', include('app_users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
