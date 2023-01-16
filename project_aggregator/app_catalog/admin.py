ef4be277ae8241cfdb242327d8721cc609c4f4e3
Андрей Зайцев <mr_zaycev@mail.ru>
2023-01-16 13:01:40 +0000

    Replace admin.py

--- project_aggregator/app_catalog/admin.py
+++ project_aggregator/app_catalog/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, PropertyProduct, ImageProduct
+
+
+# class PropertyInline(admin.TabularInline):
+#     model = PropertyProduct
+
+
+class GalleryInline(admin.TabularInline):
+    model = ImageProduct
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'type_device', 'fabricator', 'model',
+        'price', 'stock', "created", "updated", 'available', 'limited',
+    ]
+    list_editable = ['price', 'stock', 'available', 'limited']
+    prepopulated_fields = {'slug': ('type_device', 'fabricator', 'model')}
+    inlines = [GalleryInline, ]
+    fieldsets = (
+        (None, {
+            'fields': (('type_device', 'fabricator', 'model'), 'slug', )
+        }),
+        ('Характеристики и описание', {
+            'classes': ('extrapretty', 'wide'),
+            'fields': ('description', 'properties'),
+            # 'fields': ('description', 'category', 'properties'),
+        }),
+        ('Настройки продаж', {
+            'classes': ('collapse', 'wide'),
+            'fields': (('price', 'stock'), ('available', 'limited'), ),
+        }),
+    )
+    filter_horizontal = ['properties']
