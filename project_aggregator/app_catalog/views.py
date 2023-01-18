1f8d267d9753a0c38ba1f91ba312da732db93a74
Андрей Зайцев <mr_zaycev@mail.ru>
2023-01-18 08:09:53 +0000

    Replace views.py

--- project_aggregator/app_catalog/views.py
+++ project_aggregator/app_catalog/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic import TemplateView
 
+from app_configurations.models import SiteSettings
 from .models import Product
 
 
@@ -16,18 +18,18 @@ class ShopView(TemplateView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        # quantity = SiteSettings.load()
-        context['products'] = Product.objects. \
-                                  prefetch_related('order_items'). \
-                                  filter(available=True). \
-                                  only('category', 'name', 'price'). \
-                                  annotate(total=Sum('order_items__quantity')). \
-                                  order_by('-total')[:quantity.quantity_popular]
-        context['limited'] = Product.objects. \
-            select_related('category'). \
-            filter(available=True). \
-            filter(limited=True). \
-            only('category', 'name', 'price')
+        quantity = SiteSettings.load()
+        context['products'] = (
+            Product.objects.select_related('category').filter(available=True).
+            only('category', 'type_device', 'fabricator', 'model', 'price')[:quantity.quantity_top_product]
+            # Product.objects.prefetch_related('order_items').select_related('category').
+            # filter(available=True).only('category', 'get_full_name', 'price').
+            # annotate(total=Sum('order_items__quantity')).order_by('-total')[:quantity.quantity_top_product]
+        )
+        context['limited'] = (
+            Product.objects.select_related('category').
+            filter(available=True, limited=True).only('category', 'type_device', 'fabricator', 'model', 'price')
+        )
         return context
 
 
