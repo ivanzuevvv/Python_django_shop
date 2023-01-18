05bf7f16bb56b952619c0425d8b49fee093aadfb
Андрей Зайцев <mr_zaycev@mail.ru>
2023-01-18 08:07:03 +0000

    дерево категорий

---
+++ project_aggregator/app_catalog/context_processors.py
@@ -0,0 +1,10 @@
+from app_configurations.models import SiteSettings
+
+
+def load_menu(request):
+    try:
+        settings = SiteSettings.load()
+        sub_categories = settings.root_category.get_descendants(include_self=False)
+    except AttributeError as exc:
+        sub_categories = {}
+    return {'main_menu': sub_categories}
