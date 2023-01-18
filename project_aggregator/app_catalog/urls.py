855bf9c3d28edd835a219a51cafa64eef7074c60
Андрей Зайцев <mr_zaycev@mail.ru>
2023-01-18 08:10:35 +0000

    дерево категорий

--- project_aggregator/app_catalog/urls.py
+++ project_aggregator/app_catalog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='main'),
+    path('', ShopView.as_view(), name='main'),
     path('account/', AccountView.as_view(), name='account'),
 ]
