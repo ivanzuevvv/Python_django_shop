143d405bf52117c64a040bd386ae3a0b2e0932ea
Zaycev Andrey <mr_zaycev@mail.ru>
2023-01-20 22:11:01 +0300

    инит задачи

--- project_aggregator/app_catalog/views.py
+++ project_aggregator/app_catalog/views.py
@@ -3,7 +3,7 @@ from django.db.models import Sum
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import generic
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
 from app_configurations.models import SiteSettings
 from .models import Product
@@ -36,3 +36,36 @@ class ShopView(TemplateView):
 class AccountView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'app_catalog/account.html'
     raise_exception = True
+
+
+class ProductDetalView(DetailView):
+    model = Product
+    template_name = 'app_catalog/product.html'
+    context_object_name = 'product'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['review_form'] = ReviewForm()
+    #     context['form'] = CartAddProductForm(initial={'quantity': 1})
+    #     context['reviews'] = Review.objects.prefetch_related('user').filter(product=self.get_object()).order_by(
+    #         '-created')
+    #     return context
+    #
+    # def get_object(self, queryset=None):
+    #     time_cache = SiteSettings.load().time_cache_product
+    #     if not time_cache:
+    #         time_cache = 1
+    #     return cache.get_or_set(f'product:{self.kwargs.get("pk")}',
+    #                             Product.objects.get(id=self.kwargs.get("pk")), 60 * 60 * time_cache)
+    #
+    # def post(self, request, pk, slug):
+    #     form = ReviewForm(request.POST)
+    #     if not request.user.is_authenticated:
+    #         raise PermissionDenied()
+    #     if form.is_valid:
+    #         review = form.save(commit=False)
+    #         review.user = request.user
+    #         review.product = self.get_object()
+    #         review.save()
+    #         messages.success(request, 'Отзыв добавлен')
+    #     return HttpResponseRedirect(reverse('product_detail', kwargs={"pk": pk, "slug": slug}))
