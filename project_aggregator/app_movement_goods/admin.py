35b36af29007bfc9188e7e3a7410dfa7d68a2f1d
Андрей Зайцев <mr_zaycev@mail.ru>
2023-01-25 12:27:18 +0000

    настроена админка корзин

--- project_aggregator/app_movement_goods/admin.py
+++ project_aggregator/app_movement_goods/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserCart, InsideCart
+
+
+class InsideCartInline(admin.TabularInline):
+    model = InsideCart
+    verbose_name = 'Товар'
+    verbose_name_plural = "Товары в корзине"
+
+
+@admin.register(UserCart)
+class UserCartAdmin(admin.ModelAdmin):
+    inlines = [InsideCartInline]
