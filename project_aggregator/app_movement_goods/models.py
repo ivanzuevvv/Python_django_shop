37ea80a6bee6494a803ce0a2cecd155a27054587
Андрей Зайцев <mr_zaycev@mail.ru>
2023-01-25 12:26:19 +0000

    настроена админка корзин

--- project_aggregator/app_movement_goods/models.py
+++ project_aggregator/app_movement_goods/models.py
@@ -10,16 +10,16 @@ class UserCart(models.Model):
     owner = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
         related_name='cart', verbose_name='Чья корзина', blank=True)
-    session = models.TextField(verbose_name='Сессия', default='')
+    session = models.CharField(verbose_name='Сессия', default='', max_length=40)
     cart = models.ManyToManyField(
         Product, through='InsideCart', verbose_name='Содержание корзины',
         related_name="carts", blank=True)
 
     def __str__(self):
-        return 'Корзина ' + str(self.owner) if self.owner else 'Anonymous'
+        return 'Корзина ' + str(self.owner.email) if self.owner else 'Anonymous'
 
     def add(self, product, quantity=1, update_quantity=False):
-        cart = self.compound.get_or_create(user_cart=self, goods=product)[0]
+        cart = self.contents.get_or_create(user_cart=self, goods=product)[0]
         if update_quantity:
             cart.quantity = quantity
         else:
@@ -32,24 +32,28 @@ class UserCart(models.Model):
 
     def get_total_price(self):
         return sum(
-            Decimal(item.cost) * item.quantity for item in self.compound.all())
+            Decimal(item.cost) * item.quantity for item in self.contents.all())
 
     def clear(self):
         for item in self.cart.all():
             print(item)
             self.cart.remove(item)
 
+    class Meta:
+        verbose_name = "Корзина пользователя"
+        verbose_name_plural = "Корзины пользователей"
+
 
 class InsideCart(models.Model):
     user_cart = models.ForeignKey(UserCart, on_delete=models.CASCADE, related_name='contents')
-    goods = models.ForeignKey(Product, on_delete=models.CASCADE)
+    goods = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Товары')
     quantity = models.PositiveSmallIntegerField(verbose_name='Количество', default=0)
     cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Стоимость', null=True)
 """
 from app_movement_goods.models import UserCart
 from app_catalog.models import Product
 from app_users.models import User
-user = User.objects.get(pk=7)
+user = User.objects.get(pk=9)
 prod1 = Product.objects.get(pk=10)
 prod2 = Product.objects.get(pk=15)
 prod3 = Product.objects.get(pk=23)
