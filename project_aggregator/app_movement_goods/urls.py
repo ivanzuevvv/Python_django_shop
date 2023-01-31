39a1f2cd1720c71a834c83975f65b0abfdf8fd75
Андрей Зайцев <mr_zaycev@mail.ru>
2023-01-31 12:17:16 +0000

    Replace urls.py

--- project_aggregator/app_movement_goods/urls.py
+++ project_aggregator/app_movement_goods/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('add/<int:pk>/', cart_add, name='cart_add'),
     path('remove/<int:pk>/', cart_remove, name='cart_remove'),
     path('get_data/', get_cart_data, name='get_cart_data'),
-    path('get_data/', get_cart_data, name='get_cart_data'),
     path('order/', OrderView.as_view(), name='create_order'),
+    path('<int:pk>/', OrderDetail.as_view(), name='order_detail'),
+    path('history/', HistoryOrders.as_view(), name='history'),
 ]
