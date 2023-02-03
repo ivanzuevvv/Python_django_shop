c2b589409cd522493ab8944b9304548085064c3f
Андрей Зайцев <mr_zaycev@mail.ru>
2023-02-03 07:45:09 +0000

    Replace apps.py

--- project_aggregator/app_comments/apps.py
+++ project_aggregator/app_comments/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppCommentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_comments'
+    verbose_name = 'Отзывы покупателей о продуктах'
