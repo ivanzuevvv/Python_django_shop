1ac304ce2d5a6929cb544899f17bf839132a6f34
Андрей Зайцев <mr_zaycev@mail.ru>
2023-02-03 07:44:51 +0000

    Replace admin.py

--- project_aggregator/app_comments/admin.py
+++ project_aggregator/app_comments/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
+from app_comments.models import ProxyProduct, CommentProduct
 
+
+@admin.register(CommentProduct)
 class CommentAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(ProxyProduct)
+class ProxyProductAdmin(admin.ModelAdmin):
+    date_hierarchy = 'comments__pub_at'
+    list_display = [
+        '__str__', 'get_count_comments',
+        'get_last_comments_pub_at',
+        'get_last_comments_author',
+    ]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(comments__isnull=False)
